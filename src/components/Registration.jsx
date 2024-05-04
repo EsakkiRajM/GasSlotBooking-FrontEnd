4bb7133d7e06210e62f4c12bac2c3d796da89af9
EsakkiRajM <esakkiraj1150@gmail.com>
2024-05-04 14:38:16 +0530

    REGISTER form api creatged

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -4,12 +4,25 @@ import { TextField } from '@mui/material';
 import './Registration.css'
 import { Link } from 'react-router-dom'
 import { ValueContext } from '../App';
+import axios from 'axios';
 
 const Registration = () => {
 
   const { registerFormState, setRegisterFormState } = useContext(ValueContext);
 
+  const apiUrl = import.meta.env.VITE_BE_URL;
+
   console.log(registerFormState, "registerFormState");
+
+  const handleFormState = async () => {
+    if (registerFormState.username && registerFormState.password && registerFormState.phonenumber) {
+      await axios.post(`${apiUrl}/registration`, {
+        username: registerFormState.username,
+        password: registerFormState.password,
+        phonenumber: registerFormState.phonenumber,
+      })
+    }
+  }
 
   return (
     <div className="background-container">
@@ -22,59 +35,83 @@ const Registration = () => {
             phonenumber: ""
           }}
 
+          validate={
+            (values) => {
+              const errors = {};
 
+              if (!values.username) {
+                errors.username = "Username is Required"
+              }
+              if (!values.password) {
+                errors.password = "Password is Required"
+              }
+              if (!values.phonenumber) {
+                errors.phonenumber = "Phone Number No is Required"
+              }
+              return errors;
+            }
 
-          onSubmit={(values) => {
-              setRegisterFormState(
-                ...registerFormState,
-                values
-              )
-            
+          }
+
+          onSubmit={(values, { resetForm }) => {
+            setRegisterFormState(
+              ...registerFormState,
+              values
+            )
+            resetForm()
+            handleFormState();
           }}
 
         >
           <Form>
-            <h4 className='text-center'>REGISTER HERE!</h4>
+            <h4 className='text-center text-primary'>REGISTER HERE !</h4>
             <div className='text-center'>
-            <Field name="username">
-              {({ field }) => (
-                <TextField
-                  {...field}
-                  id="standard-basic"
-                  label="Enter your Email"
-                  variant="standard"
-                  type="email"
-                />
-              )}
-            </Field> <br />
-            <Field name="password">
-              {({ field }) => (
-                <TextField
-                  {...field}
-                  id="standard-basic"
-                  label="Enter your Password"
-                  variant="standard"
-                  type="password"
-                />
-              )}
-            </Field> <br />
-            <Field name="phonenumber">
-              {({ field }) => (
-                <TextField
-                  {...field}
-                  id="standard-basic"
-                  label="Enter your Mob-no"
-                  variant="standard"
-                  type="number"
-                />
-              )}
-            </Field> <br /> <br />
+              <Field name="username">
+                {({ field }) => (
+                  <TextField
+                    {...field}
+                    id="standard-basic"
+                    label="Enter your Email"
+                    variant="standard"
+                    type="email"
+                  />
+                )}
+              </Field> <br />
+              <ErrorMessage name='username' component='div' className='text-danger' />
+
+              <Field name="password">
+                {({ field }) => (
+                  <TextField
+                    {...field}
+                    id="standard-basic"
+                    label="Enter your Password"
+                    variant="standard"
+                    type="password"
+                  />
+                )}
+              </Field> <br />
+              <ErrorMessage name='password' component='div' className='text-danger' />
+              <Field name="phonenumber">
+                {({ field }) => (
+                  <TextField
+                    {...field}
+                    id="standard-basic"
+                    label="Enter your Mobile No"
+                    variant="standard"
+                    type="number"
+                  />
+                )}
+              </Field> <br />
+              <ErrorMessage name='phonenumber' component='div' className='text-danger' />
+
             </div>
-            <div>
+            <div className='mt-3'>
               <p>Already you have an account <Link to={"/login"} className='link-offset-2 link-underline link-underline-opacity-0' >login</Link></p>
             </div>
             <div className='text-center'>
-              <button type='submit' className='btn btn-primary'>Register</button>
+              <button type='submit' className='btn btn-primary'
+                
+              >Register</button>
             </div> <br />
 
           </Form>
